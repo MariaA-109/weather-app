6409b82b8dc69d2879a893ccef06b86c20ab6c8b
MariaA-109 <122259443+MariaA-109@users.noreply.github.com>
2023-01-18 11:12:06 +0100

    updated apiUrl

--- src/index.js
+++ src/index.js
@@ -79,8 +79,7 @@ function displayForecast(response) {
 function getForecast(coordinates) {
   console.log(coordinates);
   let apiKey = "5201594abea9f3e38b70e65b11a80c24";
-  let apiUrl = `http://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
-  console.log(apiUrl);
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayForecast);
 }
 
